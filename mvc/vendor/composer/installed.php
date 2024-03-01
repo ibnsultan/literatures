b5ff2f4eb40d64c9d21fc5e5d3cb381db4526535
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-01 03:38:00 +0300

    simple mvc framework

---
+++ mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ibnsultan/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5f4dcc5f709bd60f29d299086d5c3b5a53f5906e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ibnsultan/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5f4dcc5f709bd60f29d299086d5c3b5a53f5906e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
